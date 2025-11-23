e60f16dc8532eca81ab08b9c8e24f96b64540f53
abduaslam <abduaslam098@gmail.com>
2025-11-23 09:06:11 +0200

    colclation and operation

---
+++ calculation and opreations /Basic Arithmetic.R
@@ -0,0 +1,17 @@
+# Addition
+2 + 3  
+# Subtraction
+5 - 1   
+# Multiplication
+4 * 6   
+# Division
+8 / 2  
+# Exponent (power)
+7 ^ 2   
+x<-5
+x^2
+# Modulus (remainder)
+10 %% 3  
+# Integer division
+
+10 %/% 3 
